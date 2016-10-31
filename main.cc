85d69496933479f8739dd38f475b81d82eee9440
David G <david.a.grs@gmail.com>
2016-10-31 23:07:04 +0100

    resize

--- main.cc
+++ main.cc
@@ -14,18 +14,21 @@ struct empty_key
 template <typename Key, typename Value, typename EmptyKey, typename Hash = std::hash<Key>, typename Equal = std::equal_to<Key>>
 struct ht
 {
+    using Container = ht<Key, Value, EmptyKey, Hash, Equal>;
     using Node = std::pair<Key, Value>;
     using iterator = void*; // TODO
 
-    ht() :
+    ht(std::size_t capacity = 16) :
       _elements(0),
-      _max_elements(16),
+      _max_elements(capacity),
       _table(std::make_unique<Node[]>(_max_elements))
     {
     }
 
     std::pair<iterator, bool> insert(Node& n)
     {
+        resize(1);
+
         assert(!Equal()(n.first, EmptyKey::value));
 
         std::size_t pos = Hash()(n.first) & (_max_elements - 1);
@@ -45,6 +48,13 @@ struct ht
     std::size_t capacity() const { return _max_elements; }
 
 private:
+    void resize(std::size_t n)
+    {
+        if (_elements + n > _max_elements / 2)
+        {
+            // TODO
+        }
+    }
     std::size_t _elements;
     std::size_t _max_elements;
     std::unique_ptr<Node[]> _table;
