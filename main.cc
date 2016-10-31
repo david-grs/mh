189b6986274dd63f0df01701ee7908152b62f376
David G <david.a.grs@gmail.com>
2016-10-31 22:22:11 +0100

    basic stuff

--- main.cc
+++ main.cc
@@ -1,5 +1,34 @@
+#include <algorithm>
+#include <string>
+#include <type_traits>
+#include <vector>
 
+#include <cassert>
+
+template <typename Key, typename Value, typename Hash = std::hash<Key>, typename Equal = std::equal_to<Key>>
+struct ht
+{
+    using pointer = std::add_pointer<Value>;
+
+    ht() :
+      _elements(0),
+      _table(10)
+    {
+    }
+
+    std::size_t size() const { return _elements; }
+    std::size_t capacity() const { return _table.size(); }
+
+private:
+    std::size_t _elements;
+    std::vector<Value> _table;
+};
 
 int main()
 {
+
+    ht<int, std::string> h;
+
+    assert(h.capacity() == 10);
+    assert(h.size() == 0);
 }
