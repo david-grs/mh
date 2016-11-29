74f3b4ddc8b8071d5b7c7aff86adc881e6bdc260
David G <david.a.grs@gmail.com>
2016-11-29 22:28:41 +0100

    mic example (2)

--- mic.cc
+++ mic.cc
@@ -10,6 +10,8 @@ int main()
     mha.insert(std::make_pair(2, 2.0));
     mha.insert(std::make_pair(3, 2.0));
     mha.insert(std::make_pair(4, 2.0));
+    mha.insert(std::make_pair(1, 2.0));
+    mha.insert(std::make_pair(2, 2.0));
 
     for (auto& a : mha)
         std::cout << a.first << " " << a.second << std::endl;
