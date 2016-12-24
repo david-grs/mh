6cce347a8cde1ad8953353a1b246d4b498570d99
David G <david.a.grs@gmail.com>
2016-12-24 19:21:40 +0100

    typemap: foreach

--- typemap.h
+++ typemap.h
@@ -14,7 +14,26 @@ struct typemap
   template <typename T>
   void set(T&& t) { std::get<get_index<T>()>(_ts) = std::forward<T>(t); }
 
+
+  template <typename F>
+  void for_each(F&& f)
+  {
+    for_each_arg<0>(std::forward<F>(f));
+  }
+
 private:
+  template <std::size_t I, typename F>
+  std::enable_if_t<I < sizeof...(Ts)>
+  for_each_arg(F&& f)
+  {
+    f(std::get<I>(_ts));
+    for_each_arg<I + 1>(std::forward<F>(f));
+  }
+
+  template <std::size_t I, typename F>
+  std::enable_if_t<I == sizeof...(Ts)>
+  for_each_arg(F&& f) {}
+
   template <typename T>
   static constexpr std::size_t get_index() { return index<std::tuple<Ts...>, T, 0>(); }
 
