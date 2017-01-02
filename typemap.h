6359a151ae4f1401a9a51d90b1cd3efdcca79c94
David G <david.a.grs@gmail.com>
2017-01-02 23:08:31 +0100

    typemap: for_each -> visit

--- typemap.h
+++ typemap.h
@@ -17,37 +17,37 @@ struct typemap
   X& get() { return std::get<get_index<X>()>(_ts); }
 
   template <typename F>
-  void for_each(F&& f)
+  void visit(F&& f)
   {
-    for_each_arg<0>(std::forward<F>(f));
+    visit_element<0>(std::forward<F>(f));
   }
 
   template <typename F>
-  void for_each(F&& f) const
+  void visit(F&& f) const
   {
-    for_each_arg<0>(std::forward<F>(f));
+    visit_element<0>(std::forward<F>(f));
   }
 
 private:
   template <std::size_t I, typename F>
   std::enable_if_t<I < sizeof...(Ts) + 1>
-  for_each_arg(F&& f)
+  visit_element(F&& f)
   {
     f(std::get<I>(_ts));
-    for_each_arg<I + 1>(std::forward<F>(f));
+    visit_element<I + 1>(std::forward<F>(f));
   }
 
   template <std::size_t I, typename F>
   std::enable_if_t<I < sizeof...(Ts) + 1>
-  for_each_arg(F&& f) const
+  visit_element(F&& f) const
   {
     f(std::get<I>(_ts));
-    for_each_arg<I + 1>(std::forward<F>(f));
+    visit_element<I + 1>(std::forward<F>(f));
   }
 
   template <std::size_t I, typename F>
   std::enable_if_t<I == sizeof...(Ts) + 1>
-  for_each_arg(F&&) const {}
+  visit_element(F&&) const {}
 
   template <typename X>
   static constexpr std::size_t get_index() { return index<std::tuple<T, Ts...>, X, 0>(); }
@@ -70,13 +70,13 @@ private:
 template <typename... Ts>
 inline std::ostream& operator<<(std::ostream& os, const typemap<Ts...>& m)
 {
-    m.for_each([&](auto x) { os << x << " "; });
+    m.visit([&](auto x) { os << x << " "; });
     return os;
 }
 
 template <typename... Ts>
 inline std::istream& operator>>(std::istream& is, typemap<Ts...>& m)
 {
-    m.for_each([&](auto& x) { is >> x; });
+    m.visit([&](auto& x) { is >> x; });
     return is;
 }
