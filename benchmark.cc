aa8764104671e34b524f41dc09bd88b710c5c2ec
David G <david.a.grs@gmail.com>
2017-01-12 22:33:55 +0100

    renaming arg

--- benchmark.cc
+++ benchmark.cc
@@ -113,22 +113,21 @@ int main(int argc, char** argv)
 {
     if (argc != 4 && argc != 5)
     {
-        std::cerr << argv[0] << ": -ht|-ha [-gen|-cmp] N [seed]" << std::endl;
+        std::cerr << argv[0] << ": -ht|-ha [-gen] N [seed]" << std::endl;
         return 1;
     }
 
     tsc_chrono::init();
 
     //auto benchmark = argv[1] == std::string("-ht") ? benchmark_ht : benchmark_ha;
-    const bool cmp = argv[2] == std::string("-cmp");
+    const bool write = argv[2] == std::string("-gen");
     //const int n = std::atoi(argv[3]);
     long unsigned seed = argc == 5 ? std::atoll(argv[4]) : std::random_device()();
 
     //std::cout << "options: n=" << n << " seed=" << seed << " gen=" << std::boolalpha << gen << std::endl;
     const std::string ref_filename("samples.ref");
 
-    std::vector<test> ref_tests = cmp ? load_ref_file(ref_filename) : std::vector<test>();
-    const bool write = true;
+    auto ref_tests = load_ref_file(ref_filename);
 
     if (write)
         unlink(ref_filename.c_str());
