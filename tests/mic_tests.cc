5c38474beda78f31fd976f2e67847a0d44080540
David G <david.a.grs@gmail.com>
2017-02-18 13:06:42 +0100

    hash_array tests: parameter typed tests

--- tests/mic_tests.cc
+++ tests/mic_tests.cc
@@ -5,9 +5,10 @@
 #include <gtest/gtest.h>
 #include <string>
 
-struct test_ha : public ::testing::Test
+template <typename Key>
+struct TestHashArray : public ::testing::Test
 {
-    explicit test_ha() :
+    explicit TestHashArray() :
         ha(empty_key("bla"), 2)
     {}
 
@@ -15,23 +16,37 @@ struct test_ha : public ::testing::Test
     ha_type ha;
 };
 
-TEST_F(test_ha, empty)
+TYPED_TEST_CASE_P(TestHashArray);
+
+TYPED_TEST_P(TestHashArray, empty)
 {
-    EXPECT_TRUE(ha.empty());
+    EXPECT_TRUE(this->ha.empty());
 
-    ha.insert(std::make_pair("foo", 1));
-    EXPECT_FALSE(ha.empty());
+    this->ha.insert(std::make_pair("foo", 1));
+    EXPECT_FALSE(this->ha.empty());
 
-    ha = ha_type(empty_key("bla"), 4);
-    EXPECT_TRUE(ha.empty());
+    this->ha = typename TestHashArray<TypeParam>::ha_type(empty_key("bla"), 4);
+    EXPECT_TRUE(this->ha.empty());
 }
 
-TEST_F(test_ha, size)
+TYPED_TEST_P(TestHashArray, size)
 {
     for (int i = 0; i < 100; ++i)
     {
-        EXPECT_EQ(i, (int)ha.size());
-        ha.insert(std::make_pair("foo" + std::to_string(i), 1));
+        EXPECT_EQ(i, (int)this->ha.size());
+        this->ha.insert(std::make_pair("foo" + std::to_string(i), 1));
     }
 }
+
+TYPED_TEST_P(TestHashArray, walk)
+{
+    int i = 0;
+    for (const auto& p : this->ha)
+        std::cout << i++ << ": " << p.first << std::endl;
+}
+
+using KeyTypes = testing::Types<std::string, int, double>;
+
+REGISTER_TYPED_TEST_CASE_P(TestHashArray, empty, size, walk);
+INSTANTIATE_TYPED_TEST_CASE_P(KeyTypes, TestHashArray, KeyTypes);
 
