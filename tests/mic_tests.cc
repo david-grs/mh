bf33b423d1be3ab467a4f38d63a178033b8e9f0f
David G <david.a.grs@gmail.com>
2017-02-18 12:52:01 +0100

    addingmic unit tests

--- tests/mic_tests.cc
+++ tests/mic_tests.cc
@@ -1,17 +1,37 @@
-#define HT_DEBUG_IO
+//#define HT_DEBUG_IO
 
 #include "mic.h"
 
 #include <gtest/gtest.h>
 #include <string>
 
-TEST(ha, empty)
+struct test_ha : public ::testing::Test
 {
-    hash_array<std::string, int> mha(empty_key("bla"), 4);
-    
-    for (int i = 0; i < 100; ++i)
-        mha.insert(std::make_pair("foo" + std::to_string(i), 1));
+    explicit test_ha() :
+        ha(empty_key("bla"), 2)
+    {}
 
+    using ha_type = hash_array<std::string, int>;
+    ha_type ha;
+};
+
+TEST_F(test_ha, empty)
+{
+    EXPECT_TRUE(ha.empty());
+
+    ha.insert(std::make_pair("foo", 1));
+    EXPECT_FALSE(ha.empty());
+
+    ha = ha_type(empty_key("bla"), 4);
+    EXPECT_TRUE(ha.empty());
 }
 
+TEST_F(test_ha, size)
+{
+    for (int i = 0; i < 100; ++i)
+    {
+        EXPECT_EQ(i, (int)ha.size());
+        ha.insert(std::make_pair("foo" + std::to_string(i), 1));
+    }
+}
 
