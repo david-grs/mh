afbb27f431aeaad9ec24c07c0d4482c49eedf83b
David G <david.a.grs@gmail.com>
2017-03-22 23:00:50 +0100

    filter: conversion back to index_seq

--- filter.h
+++ filter.h
@@ -44,11 +44,14 @@ struct filter_impl<Index, value_sequence<>>
 
 }
 
+template <std::size_t... Is>
+constexpr std::index_sequence<Is...> to_index_sequence(value_sequence<Is...>) { return {}; }
+
 template <std::size_t Index, typename Is>
 struct filter;
 
 template <std::size_t Index, std::size_t... Is>
 struct filter<Index, std::index_sequence<Is...>>
 {
-    constexpr auto operator()() { return detail::filter_impl<Index, value_sequence<Is...>>{}(); }
+    constexpr auto operator()() { return to_index_sequence(detail::filter_impl<Index, value_sequence<Is...>>{}()); }
 };
